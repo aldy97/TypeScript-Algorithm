d8a1cb71a0d3ecd1b6cac70712956dc5bccab826
Feng Xiong <fengxiong34@gmail.com>
2020-07-31 17:19:04 -0700

    TwoSum done

--- src/medium/LongestSubstringNoRepeat.ts
+++ src/medium/LongestSubstringNoRepeat.ts
@@ -10,7 +10,9 @@ function lengthOfLongestSubstring(s: string): number {
       set.push(s.charAt(j++));
       ans = Math.max(ans, j - i);
     } else {
+      console.log(set);
       set.filter((str) => str !== s.charAt(i++));
+      console.log(set);
     }
   }
   return ans;
