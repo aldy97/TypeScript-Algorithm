1e396b549963660f649f3b3101bfcb7b4b44b20f
Feng Xiong <fengxiong34@gmail.com>
2020-08-01 09:46:03 -0700

    finsihed LinkedListCycle

---
+++ src/easy/LinkedListCycle.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+function hasCycle(head: any): boolean {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+}
