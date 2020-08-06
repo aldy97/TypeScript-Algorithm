8547aaa4fa858ec484efa45c0110e747f5eb2d19
Feng Xiong <fengxiong34@gmail.com>
2020-08-06 10:18:00 -0700

    added maxSubArray

--- src/medium/LongestSubstringNoRepeat.ts
+++ src/medium/LongestSubstringNoRepeat.ts
@@ -2,9 +2,9 @@
 function lengthOfLongestSubstring(s: string): number {
   const n = s.length;
   let set: string[] = [];
-  let ans = 0,
-    i = 0,
-    j = 0;
+  let ans = 0;
+  let i = 0;
+  let j = 0;
   while (i < n && j < n) {
     if (!set.includes(s.charAt(j))) {
       set.push(s.charAt(j++));
