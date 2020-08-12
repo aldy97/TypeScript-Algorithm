898ef046aabde07123b0b05c6edd69485d8c3072
Feng Xiong <fengxiong34@gmail.com>
2020-08-11 18:37:12 -0700

    IsSymmetric

---
+++ src/easy/IsSymmetric.ts
@@ -0,0 +1,20 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function helper(left: TreeNode | null, right: TreeNode | null): boolean {
+  if (left === null || right === null) return left === right;
+  if (left.val !== right.val) return false;
+  return helper(left.left, right.right) && helper(left.right, right.left);
+}
+
+function isSymmetric(root: TreeNode | null): boolean {
+  return root === null || helper(root.left, root.right);
+}
