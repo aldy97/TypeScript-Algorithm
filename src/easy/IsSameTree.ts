607f782e6ae63b1636b0334bfbf503c6f50bc5f2
Feng Xiong <fengxiong34@gmail.com>
2020-09-01 22:31:48 -0700

    added new solution

---
+++ src/easy/IsSameTree.ts
@@ -0,0 +1,18 @@
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (p === null && q === null) return true;
+  // one of p and q is null
+  if (q === null || p === null) return false;
+  if (p.val != q.val) return false;
+  return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+}
