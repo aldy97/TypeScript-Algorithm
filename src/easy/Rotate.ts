aa4bceb432181988b19e7d34dcb0677e6ef2b729
Feng Xiong <fengxiong34@gmail.com>
2020-09-11 12:25:40 -0700

    added rotate

---
+++ src/easy/Rotate.ts
@@ -0,0 +1,19 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function reverse(nums: number[], start: number, end: number) {
+  while (start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
+}
+
+function rotate(nums: number[], k: number): void {
+  k %= nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+}
