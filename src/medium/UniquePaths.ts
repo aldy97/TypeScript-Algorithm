1da3b3d8b80a958bdca26bb1dae422b81b46ea38
Feng Xiong <fengxiong34@gmail.com>
2020-09-10 19:33:44 -0700

    added new sln

---
+++ src/medium/UniquePaths.ts
@@ -0,0 +1,15 @@
+function uniquePaths(m: number, n: number): number {
+  const dp = new Array(m + 1).fill(0);
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(n + 1).fill(1);
+  }
+
+  dp[m][n] = 1;
+  for (let row = m - 1; row > 0; row--) {
+    for (let col = n - 1; col > 0; col--) {
+      dp[row][col] = dp[row + 1][col] + dp[row][col + 1];
+    }
+  }
+
+  return dp[1][1];
+}
