c62e59d504eb5e1afb28e21ee5bed1b8b584bcb7
Feng Xiong <fengxiong34@gmail.com>
2020-09-11 11:21:31 -0700

    added new solution

--- src/medium/UniquePaths.ts
+++ src/medium/UniquePaths.ts
@@ -1,8 +1,10 @@
 function uniquePaths(m: number, n: number): number {
   const dp = new Array(m + 1).fill(0);
+  console.log(dp);
   for (let i = 0; i < dp.length; i++) {
     dp[i] = new Array(n + 1).fill(1);
   }
+  console.log(dp);
 
   dp[m][n] = 1;
   for (let row = m - 1; row > 0; row--) {
@@ -13,3 +15,5 @@ function uniquePaths(m: number, n: number): number {
 
   return dp[1][1];
 }
+
+console.log(uniquePaths(3, 2));
