81ffd66af60cb2a11017b4b35727561300de9252
Feng Xiong <fengxiong34@gmail.com>
2020-09-26 15:26:15 -0700

    searchInSortedArray

---
+++ src/medium/SearchInSortedArray.ts
@@ -0,0 +1,26 @@
+//O(logn) solution: binary search, there is ALWAYS one part that is sorted
+//We can only confirm whether target exists in a sorted part
+function searchInSortedArray(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target <= nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (nums[mid] <= target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+}
