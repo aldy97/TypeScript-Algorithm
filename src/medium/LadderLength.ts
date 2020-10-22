6f9556374ff72af2a18eea92591e091cb1b057e4
Feng Xiong <fengxiong34@gmail.com>
2020-10-21 22:47:17 -0700

    LadderLength

---
+++ src/medium/LadderLength.ts
@@ -0,0 +1,41 @@
+function getDifference(A: string, B: string) {
+  let count = 0;
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] !== B[i]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function ladderLength(
+  beginWord: string,
+  endWord: string,
+  wordList: string[]
+): number {
+  const dict = new Set(wordList);
+  let step = 1;
+  let q = [beginWord];
+
+  while (q.length) {
+    const next: any = [];
+    for (let w of q) {
+      if (w === endWord) return step;
+
+      for (let word of dict) {
+        if (getDifference(word, w) === 1) {
+          next.push(word);
+          dict.delete(word);
+        }
+      }
+    }
+    q = next;
+    step++;
+  }
+
+  return 0;
+}
+
+console.log(
+  ladderLength('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog'])
+);
