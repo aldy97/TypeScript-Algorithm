ca4a2ebdc77e8c6e332d59605c8a8c2c517e63ef
Feng Xiong <fengxiong34@gmail.com>
2020-10-25 08:44:53 -0700

    added combine

---
+++ src/medium/Combine.ts
@@ -0,0 +1,19 @@
+function combine(n: number, k: number): number[][] {
+  if (n === 1 && k === 1) {
+    return [[1]];
+  }
+  let results: number[][] = [];
+  function backtrack(max: number, curr: number[], index: number) {
+    if (curr.length === max) {
+      results.push(curr);
+      console.log('results: ', results);
+      return;
+    } else {
+      while (index <= n) {
+        backtrack(max, [...curr, index], ++index);
+      }
+    }
+  }
+  backtrack(k, [], 1);
+  return results;
+}
