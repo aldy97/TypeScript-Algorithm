85167d17265f2b086d3ec4d80c73cabfd3df1c6c
Feng Xiong <fengxiong34@gmail.com>
2020-10-26 10:19:21 -0700

    added rightSlideView

---
+++ src/medium/RightSideView.ts
@@ -0,0 +1,28 @@
+import {TreeNode} from '../easy/IsSameTree';
+function rightSideView(root: TreeNode | null): number[] {
+  if (!root) {
+    return [];
+  }
+  let result: number[] = [root.val];
+  let queue: TreeNode[] = [root];
+  while (queue.length !== 0) {
+    let currLevel: number[] = [];
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const curr = queue.shift() as TreeNode; 
+      if (curr.left) {
+        queue.push(curr.left);
+        currLevel.push(curr.left.val);
+      } 
+      if (curr.right) {
+        queue.push(curr.right);
+        currLevel.push(curr.right.val);
+      }
+    }
+    const number = currLevel.pop();
+    if (number) {
+      result.push(number);
+    }
+  }
+  return result;
+};
