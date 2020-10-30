1c5a7bbca5a2b4e7608d7aaf156f331080dda3e6
Feng Xiong <fengxiong34@gmail.com>
2020-10-30 14:02:02 -0700

    added setZerosMatrix

---
+++ src/medium/SetMatrixZeros.ts
@@ -0,0 +1,23 @@
+//Space complexity: O(mn) in worst case in which every single ele is 0 in the matrix
+function setZeroes(matrix: number[][]): void {
+  const height = matrix.length;
+  const length = matrix[0].length;
+  const zeros = [];
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < length; j++) {
+      if (matrix[i][j] === 0) {
+        zeros.push([i, j]);
+      }
+    }
+  }
+  for (let pair of zeros) {
+    const yCor = pair[0];
+    const xCor = pair[1];
+    for (let i = 0; i < height; i++) {
+      matrix[i][xCor] = 0;
+    }
+    for (let i = 0; i < length; i++) {
+      matrix[yCor][i] = 0;
+    }
+  }
+}
