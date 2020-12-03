6db16db3e5e5bba30a3c7c663dbefc98769ce95a
Feng Xiong <fengxiong34@gmail.com>
2020-12-03 06:10:05 -0800

    Letter Combination on Num Pad

--- src/medium/LetterCombinationOnNumPad.ts
+++ src/medium/LetterCombinationOnNumPad.ts
@@ -2,22 +2,32 @@ type mapProps = {
   [key: number]: string;
 };
 const map: mapProps = {
-  2: 'abc',
-  3: 'def',
-  4: 'ghi',
-  5: 'jkl',
-  6: 'mno',
-  7: 'pqrs',
-  8: 'tuv',
-  9: 'wxyz',
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz",
 };
 
 function letterCombinations(digits: string): string[] {
+  if (digits.length === 0) {
+    return [];
+  }
   let result: string[] = [];
-  const prep = [...digits].map((char) => parseInt(char)).map((num) => map[num]);
-  console.log(prep);
-
+  function backtrack(index: number, curr: string) {
+    if (index === digits.length) {
+      result.push(curr);
+      return;
+    }
+    for (const char of map[parseInt(digits[index])]) {
+      backtrack(index + 1, curr + char);
+    }
+  }
+  backtrack(0, "");
   return result;
 }
 
-console.log(letterCombinations('23'));
+console.log(letterCombinations("23"));
