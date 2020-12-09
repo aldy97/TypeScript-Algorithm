8e2adbfbaac8f72d198b2bfedfcc896fdb17d132
Feng Xiong <fengxiong34@gmail.com>
2020-12-09 14:45:15 -0800

    Course Schedule

--- src/medium/CanFinish.ts
+++ src/medium/CanFinish.ts
@@ -1,64 +1,113 @@
+// function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+//   if (numCourses === 0) {
+//     return true;
+//   }
+
+//   let courseWithPreq = new Map<number, number[]>();
+//   for (const pair of prerequisites) {
+//     const preq = courseWithPreq.get(pair[0]) || [];
+//     courseWithPreq.set(pair[0], [...preq, pair[1]]);
+//   }
+
+//   function backtrack(taken: number[], curr: number) {
+//     let added = false;
+//     if (!courseWithPreq.has(curr)) {
+//       taken.push(curr);
+//       added = true;
+//     } else {
+//       let meetReq = true;
+//       for (const course of courseWithPreq.get(curr) as number[]) {
+//         if (!taken.includes(course)) {
+//           meetReq = false;
+//           break;
+//         }
+//       }
+
+//       if (meetReq) {
+//         taken.push(curr);
+//         added = true;
+//       }
+//     }
+
+//     if (taken.length === numCourses) {
+//       return true;
+//     }
+
+//     if (added) {
+//       for (let i = 0; i < numCourses; i++) {
+//         if (!taken.includes(i)) {
+//           if (backtrack(taken, i)) {
+//             return true;
+//           }
+//         }
+//       }
+//     }
+//   }
+
+//   for (let i = 0; i < numCourses; i++) {
+//     if (backtrack([], i)) {
+//       return true;
+//     }
+//   }
+
+//   return false;
+// }
+
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
   if (numCourses === 0) {
     return true;
   }
 
-  //Make list of remaining courses using Set
-  let courseList = new Set<number>();
-  for (let i = 0; i < numCourses; i++) {
-    courseList.add(i);
+  // build a map as course as key and all preReq as value
+  let courseWithPreq = new Map<number, number[]>();
+  for (const pair of prerequisites) {
+    const preq = courseWithPreq.get(pair[0]) || [];
+    courseWithPreq.set(pair[0], [...preq, pair[1]]);
   }
 
-  let courseWithPrereqList = new Set<number>();
-  for (let pair of prerequisites) {
-    if (!courseWithPrereqList.has(pair[0])) {
-      courseWithPrereqList.add(pair[0]);
-    }
-  }
-
-  //if every single course has a pre-req in the first place, then it returns false
-  if (courseWithPrereqList.size === numCourses) {
-    return false;
-  } else if (courseWithPrereqList.size === 0) {
-    return true;
-  }
-
-  //initialize courses without pre-req and delet these from courseList
-  let coursesCanBeTakenList = [];
-  for (let course of courseList) {
-    if (!courseWithPrereqList.has(course)) {
-      coursesCanBeTakenList.push(course);
-      courseList.delete(course);
-    }
-  }
-
-  while (coursesCanBeTakenList.length !== 0) {
-    if (courseList.size === 0) {
+  function backtrack(visited: Set<number>, unVisited: Set<number>) {
+    if (visited.size === numCourses) {
       return true;
     }
-    console.log('courseList: ', courseList);
-    console.log('coursesCanBeTakenList: ', coursesCanBeTakenList);
-    const size = coursesCanBeTakenList.length;
-    for (let i = 0; i < size; i++) {
-      const currCourse = coursesCanBeTakenList.shift();
-      for (let pair of prerequisites) {
-        if (
-          pair[1] === currCourse &&
-          !coursesCanBeTakenList.includes(pair[0])
-        ) {
-          coursesCanBeTakenList.push(pair[0]);
-          courseList.delete(pair[0]);
+
+    for (const course of unVisited) {
+      if (!courseWithPreq.get(course)) {
+        visited.add(course);
+        unVisited.delete(course);
+        if (backtrack(visited, unVisited)) {
+          return true;
+        }
+        visited.delete(course);
+        unVisited.add(course);
+      } else {
+        const required = courseWithPreq.get(course) as number[];
+        let metReq = true;
+        for (const c of required) {
+          if (!visited.has(c)) {
+            metReq = false;
+            break;
+          }
+        }
+        if (metReq) {
+          visited.add(course);
+          unVisited.delete(course);
+          if (backtrack(visited, unVisited)) {
+            return true;
+          }
+          visited.delete(course);
+          unVisited.add(course);
         }
       }
     }
-  }
-  return courseList.size === 0;
-}
 
-console.log(
-  canFinish(3, [
-    [1, 0],
-    [1, 2],
-    [0, 1],
-  ])
-);
+    return false;
+  }
+
+  let arr: number[] = [];
+  for (let i = 0; i < numCourses; i++) {
+    arr.push(i);
+  }
+  const visited = new Set<number>();
+  const unVisited = new Set<number>(arr);
+  return backtrack(visited, unVisited);
+}
