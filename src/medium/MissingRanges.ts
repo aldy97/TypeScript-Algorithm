7d14dd39820f28bbaa4b9b2030c7b390ca25ff61
Feng Xiong <fengxiong34@gmail.com>
2020-12-11 08:16:12 -0800

    daily challenge: MissingRanges

---
+++ src/medium/MissingRanges.ts
@@ -0,0 +1,24 @@
+// Time and Space: O(n)
+function findMissingRanges(
+  nums: number[],
+  lower: number,
+  upper: number
+): string[] {
+  let pointer = 0;
+  let ans = [];
+  let numSet = new Set(nums);
+  for (let i = lower; i <= upper; i++) {
+    if (!numSet.has(i) && nums[pointer] !== undefined) {
+      ans.push([i, nums[pointer] - 1]);
+      i = nums[pointer];
+    } else if (!numSet.has(i) && nums[pointer] === undefined) {
+      ans.push([i, upper]);
+      i = upper;
+    }
+    pointer++;
+  }
+  const res = ans.map((arr) =>
+    arr.length === 2 && arr[0] !== arr[1] ? `${arr[0]}->${arr[1]}` : `${arr[0]}`
+  );
+  return res;
+}
