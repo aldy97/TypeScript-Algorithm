3114a6aee8c94ae59a293665dc4e99df22ba6a0a
Feng Xiong <fengxiong34@gmail.com>
2020-12-16 06:44:02 -0800

    rewind: is Valid Binary Tree

--- src/medium/IsBst.ts
+++ src/medium/IsBst.ts
@@ -1,4 +1,4 @@
-import { TreeNode } from '../easy/IsSameTree';
+import { TreeNode } from "../easy/IsSameTree";
 
 //one should keep both upper and lower limits for each node while traversing the tree, and compare the node value not with children values but with these limits.
 function isValidBST(root: TreeNode | null): boolean {
@@ -18,4 +18,29 @@ function isValidBST(root: TreeNode | null): boolean {
     );
   }
   return helper(root, null, null);
+}
+
+function isValidBST2(root: TreeNode | null): boolean {
+  function checkIsValid(
+    root: TreeNode | null,
+    lower: number | null,
+    upper: number | null
+  ): boolean {
+    if (!root) {
+      return true;
+    }
+    // caution: lower? returns false when lower = 0
+    const newL = lower === null ? -Infinity : lower;
+    const newU = upper === null ? Infinity : upper;
+    if (root.val < newU && root.val > newL) {
+      return (
+        checkIsValid(root.left, lower, root.val) &&
+        checkIsValid(root.right, root.val, upper)
+      );
+    } else {
+      return false;
+    }
+  }
+
+  return checkIsValid(root, null, null);
 }
