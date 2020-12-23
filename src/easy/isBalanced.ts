c6305ea52020d0802b1f2fc0a66dea4fdc75eef7
Feng Xiong <fengxiong34@gmail.com>
2020-12-23 08:12:55 -0800

    redo isBalancedBinaryTree

--- src/easy/isBalanced.ts
+++ src/easy/isBalanced.ts
@@ -1,21 +1,40 @@
-import { TreeNode } from './IsSameTree';
+import { TreeNode } from "./IsSameTree";
 
-function height(node: TreeNode | null) {
-  if (node == null) {
-    /* base case */
+// function height(node: TreeNode | null) {
+//   if (node == null) {
+//     /* base case */
+//     return 0;
+//   }
+//   let left: number = height(node.left);
+//   let right: number = height(node.right);
+
+//   return Math.max(left, right) + 1;
+// }
+
+// function isBalanced(root: TreeNode | null): boolean {
+//   return (
+//     root == null ||
+//     (isBalanced(root.left) &&
+//       isBalanced(root.right) &&
+//       Math.abs(height(root.left) - height(root.right)) < 2)
+//   );
+// }
+function getHeight(root: TreeNode | null): number {
+  if (!root) {
     return 0;
   }
-  let left: number = height(node.left);
-  let right: number = height(node.right);
-
-  return Math.max(left, right) + 1;
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right));
 }
 
 function isBalanced(root: TreeNode | null): boolean {
-  return (
-    root == null ||
-    (isBalanced(root.left) &&
-      isBalanced(root.right) &&
-      Math.abs(height(root.left) - height(root.right)) < 2)
-  );
+  if (!root) {
+    return true;
+  }
+  const LH = getHeight(root.left);
+  const RH = getHeight(root.right);
+  if (Math.abs(LH - RH) > 1) {
+    return false;
+  } else {
+    return isBalanced(root.left) && isBalanced(root.right);
+  }
 }
