5a1274ceeab52058536c932c228182257f6eb604
Feng Xiong <fengxiong34@gmail.com>
2021-01-12 08:56:48 -0800

    modified solution

--- src/easy/MergTwoSortedList.ts
+++ src/easy/MergTwoSortedList.ts
@@ -1,7 +1,14 @@
-function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-  var insertPos = m + n - 1;
+// Insight: insert value backwards in order to avoid overflow
+// Time: O(m + n)
+// Space: O(1)
+function merge2(nums1: number[], m: number, nums2: number[], n: number): void {
+  // the index at which we start to insert value
+  let insertPos = m + n - 1;
+
   m--;
   n--;
+
+  // In JavaScript, any Number > undefined is true
   while (n >= 0) {
     nums1[insertPos--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
   }
