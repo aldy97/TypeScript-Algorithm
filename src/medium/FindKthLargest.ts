ca41c20918632a7d5fc38d4d1eb7b73f3832b563
Feng Xiong <fengxiong34@gmail.com>
2021-01-16 17:03:34 -0800

    KthLargest

--- src/medium/FindKthLargest.ts
+++ src/medium/FindKthLargest.ts
@@ -1,10 +1,40 @@
+// Brute Force
+// Time: O(nlogn)
+// Space: O(1)
 function findKthLargest(nums: number[], k: number): number {
-  if (nums.length < k) return -1;
-  nums.sort((a, b) => a - b);
-  for (let i = 0; i < k - 1; i++) {
-    nums.pop();
-  }
-  return nums[nums.length - 1];
+  return nums.sort((a, b) => a - b)[nums.length - k];
 }
 
-console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+// Quick sort
+// Time: O(n) in average, O(n^2) in worst case
+// Space: O(1)
+function findKthLargest2(nums: number[], k: number): number {
+  function helper(start: number, end: number, k: number): number {
+    let i = start;
+    const pivot = nums[end];
+
+    for (let j = start; j < end; j++) {
+      if (nums[j] > pivot) {
+        const temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        i++;
+      }
+    }
+
+    const temp = nums[i];
+    nums[i] = pivot;
+    nums[end] = temp;
+
+    const count = i - start + 1;
+    if (count === k) {
+      return nums[i];
+    } else if (count < k) {
+      return helper(i + 1, end, k - count);
+    } else {
+      return helper(start, i - 1, k);
+    }
+  }
+
+  return helper(0, nums.length - 1, k);
+}
