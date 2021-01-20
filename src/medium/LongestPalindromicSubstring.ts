7e51df4f77afbdb94763f7b94c9071098a43afad
Feng Xiong <fengxiong34@gmail.com>
2021-01-20 13:15:34 -0800

    redo longestPalindrome

--- src/medium/LongestPalindromicSubstring.ts
+++ src/medium/LongestPalindromicSubstring.ts
@@ -1,24 +1,30 @@
-//helper:deals with boses cases in which length of palindrome is even/odd.
-function palindrome(s: string, left: number, right: number) {
-  //boundary caution
-  while (left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+// helper: expand around the center(s)
+function expandAroundCenter(s: string, left: number, right: number): string {
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
     left--;
     right++;
   }
+
   return s.substr(left + 1, right - left - 1);
 }
 
+// Insight: we have total of 2n - 1 centers for odd len and even len
+// Time: O(n^2);
+// Space: O(1);
 function longestPalindrome(s: string): string {
-  //base case:
-  if (s.length === 0 || s.length === 1) return s;
-  let result: string = '';
+  let result = "";
+
   for (let i = 0; i < s.length; i++) {
     //s[i] centered: odd length
-    let s1 = palindrome(s, i, i);
+    let s1 = expandAroundCenter(s, i, i);
     //s[i], s[i+1] centered: even length
-    let s2 = palindrome(s, i, i + 1);
+    let s2 = expandAroundCenter(s, i, i + 1);
+
     result = result.length > s1.length ? result : s1;
     result = result.length > s2.length ? result : s2;
   }
+
   return result;
 }
+
+console.log(longestPalindrome("bababadbabad"));
