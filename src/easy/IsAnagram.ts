5839b4046c4e3696311974781efd9a578ccf4a2d
Feng Xiong <fengxiong34@gmail.com>
2021-02-11 21:30:12 -0500

    modified isAnagramm

--- src/easy/IsAnagram.ts
+++ src/easy/IsAnagram.ts
@@ -1,13 +1,27 @@
+// Time: O(Math.max(n,m))
+// Space: O(n + m)
 function isAnagram(s: string, t: string): boolean {
-  if (s.length !== t.length) return false;
-  const map: any = {};
-  for (let c of s) {
-    if (map[c] == null) map[c] = 0;
-    map[c]++;
+  let map1 = new Map<string, number>();
+  let map2 = new Map<string, number>();
+  for (const char of s) {
+    const num = map1.get(char) || 0;
+    map1.set(char, num + 1);
   }
-  for (let c of t) {
-    if (map[c] > 0) map[c]--;
-    else return false;
+
+  for (const char of t) {
+    const num = map2.get(char) || 0;
+    map2.set(char, num + 1);
   }
+
+  if (map1.size !== map2.size) {
+    return false;
+  }
+
+  for (const [k, v] of map1) {
+    if (map2.get(k) !== v) {
+      return false;
+    }
+  }
+
   return true;
 }
