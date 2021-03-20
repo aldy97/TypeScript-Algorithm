1d26473a3fa2ca9f74c5774de785553fd10b7737
Feng Xiong <fengxiong34@gmail.com>
2021-03-19 21:20:39 -0400

    keys and rooms

---
+++ src/medium/KeysAndRooms.ts
@@ -0,0 +1,47 @@
+// Insights: DFS, my original work
+// Time: O(n + e), n is number of rooms and e is number of keys
+// Space: O(n)
+function canVisitAllRooms(rooms: number[][]): boolean {
+  if (rooms.length < 2) return true;
+
+  let visited = new Set<number>([0]);
+  let keys = [...rooms[0]];
+
+  while (keys.length) {
+    const key = keys.pop();
+    if (!key) {
+      return false;
+    } else {
+      visited.add(key);
+      const accessible = rooms[key];
+      for (const room of accessible) {
+        if (!visited.has(room)) {
+          keys.push(room);
+        }
+      }
+
+      if (visited.size === rooms.length) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+// Written by others
+function canVisitAllRooms2(rooms: number[][]): boolean {
+  const visited = new Set();
+  const access = [0];
+
+  while (access.length) {
+    const roomId = access.pop() as number;
+
+    if (visited.has(roomId)) continue;
+
+    visited.add(roomId);
+    access.push(...rooms[roomId]);
+  }
+
+  return rooms.length === visited.size;
+}
