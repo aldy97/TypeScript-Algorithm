5c95aba877a8b603138063ad076f766693834c1d
Feng Xiong <fengxiong34@gmail.com>
2021-04-02 13:55:28 -0400

    max form

---
+++ src/medium/OnesAndZeroes.ts
@@ -0,0 +1,20 @@
+// Insights: 2-d dp, dp[i][j] denotes max when number of zeroes is i and number of ones is j
+// Time: O(l * m * n);
+// Space: O(m * n)
+function findMaxForm(S: string[], M: number, N: number) {
+  let dp = Array.from({ length: M + 1 }, () => new Uint8Array(N + 1));
+
+  for (let i = 0; i < S.length; i++) {
+    let str = S[i],
+      zeros = 0,
+      ones = 0;
+    for (let j = 0; j < str.length; j++)
+      str.charAt(j) === "0" ? zeros++ : ones++;
+
+    for (let j = M; j >= zeros; j--)
+      for (let k = N; k >= ones; k--)
+        dp[j][k] = Math.max(dp[j][k], dp[j - zeros][k - ones] + 1);
+  }
+
+  return dp[M][N];
+}
