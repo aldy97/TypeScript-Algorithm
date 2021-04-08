036f77a0419294beeec6614c31d8610268f4d664
Feng Xiong <fengxiong34@gmail.com>
2021-04-07 23:48:36 -0400

    halvesAreLike

--- src/medium/HalvesAreLike.ts
+++ src/medium/HalvesAreLike.ts
@@ -4,16 +4,43 @@ function halvesAreAlike1(s: string): boolean {
   if (!s.length) return true;
 
   s = s.toLowerCase();
-  const vowels = ["a", "e", "i", "o", "u"];
+  const vowels = new Set<string>(["a", "e", "i", "o", "u"]);
   const first = s.slice(0, s.length / 2);
   const second = s.slice(s.length / 2);
 
   return (
     [...first].filter((char) => {
-      return vowels.includes(char);
+      return vowels.has(char);
     }).length ===
     [...second].filter((char) => {
-      return vowels.includes(char);
+      return vowels.has(char);
     }).length
   );
+}
+
+// Time: O(n)
+// Space: O(1)
+function halvesAreAlike2(s: string): boolean {
+  if (!s.length) return true;
+
+  s = s.toLowerCase();
+  let left = 0,
+    right = s.length - 1,
+    c1 = 0,
+    c2 = 0;
+  const set = new Set<string>(["a", "e", "i", "o", "u"]);
+
+  while (left < right) {
+    if (set.has(s[left])) {
+      c1++;
+    }
+    if (set.has(s[right])) {
+      c2++;
+    }
+
+    left++;
+    right--;
+  }
+
+  return c1 === c2;
 }
