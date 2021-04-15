b8b1fd60272fa6bdc182ddadde36020e3583de79
Feng Xiong <fengxiong34@gmail.com>
2021-04-14 20:00:09 -0400

    redo partition list

--- src/medium/PartitionLabel.ts
+++ src/medium/PartitionLabel.ts
@@ -1,19 +1,26 @@
-function partitionLabels(S: string): number[] {
-  let map = new Map<string, number>();
-  let start = 0;
-  let end = 0;
-  let result: number[] = [];
-  for (let i = 0; i < S.length; i++) {
-    map.set(S[i], i);
-  }
+import { ListNode } from "../medium/AddTwoNumbers";
 
-  for (let i = 0; i < S.length; i++) {
-    end = Math.max(end, map.get(S[i]) as number);
-    if (i == end) {
-      result.push(end - start + 1);
-      start = i + 1;
+// Insights: all nodes with val less than x puts in left, else in right
+// Time: O(n)
+// Space: O(1), left and right are built just by composition of given head
+function partition(head: ListNode | null, x: number): ListNode | null {
+  let left = new ListNode(0);
+  let pt1 = left;
+  let right = new ListNode(0);
+  let pt2 = right;
+
+  while (head !== null) {
+    if (head.val < x) {
+      pt1.next = head;
+      pt1 = pt1.next;
+    } else {
+      pt2.next = head;
+      pt2 = pt2.next;
     }
+    head = head.next;
   }
 
-  return result;
+  pt2.next = null;
+  pt1.next = right.next;
+  return left.next;
 }
