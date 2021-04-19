5fd29c2917e5142d4c59254905775eed54e6a862
Feng Xiong <fengxiong34@gmail.com>
2021-04-18 21:04:09 -0400

    redo remove Nth fron end

--- src/easy/RemoveNFromBack.ts
+++ src/easy/RemoveNFromBack.ts
@@ -1,24 +1,22 @@
-import { ListNode } from '../medium/AddTwoNumbers';
-
+import { ListNode } from "../medium/AddTwoNumbers";
+// Insights: second node is n nodes aways from first
+// Time: O(n), where n is number of nodes
+// Space: O(1)
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
   let dummy = new ListNode(0);
   dummy.next = head;
-  let length = 0;
-  let first = head;
+  let first: ListNode | null = dummy;
+  let second: ListNode | null = dummy;
+
+  for (let i = 1; i <= n + 1; i++) {
+    first = first?.next || null;
+  }
+
   while (first != null) {
-    length++;
     first = first.next;
+    second = (second as ListNode).next;
   }
-  length -= n;
-  first = dummy;
-  while (length > 0) {
-    length--;
-    if (first && first.next) {
-      first = first.next;
-    }
-  }
-  if (first && first.next && first.next.next) {
-    first.next = first.next.next;
-  }
+
+  (second as ListNode).next = second?.next?.next || null;
   return dummy.next;
 }
