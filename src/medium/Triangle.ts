22b643844c755cada5028e5076537725bfcc8bd1
Feng Xiong <fengxiong34@gmail.com>
2021-04-21 18:09:35 -0400

    redo triangle

--- src/medium/Triangle.ts
+++ src/medium/Triangle.ts
@@ -1,3 +1,5 @@
+// Time: O(n^2), where n is number of rows
+// Space: O(1), changes are made directly on give input
 function minimumTotal(triangle: number[][]): number {
   if (triangle.length === 0) {
     return Math.max(...triangle[0]);
@@ -15,4 +17,28 @@ function minimumTotal(triangle: number[][]): number {
     }
   }
   return Math.min(...triangle[triangle.length - 1]);
+}
+
+// Insights: top-down dp table
+// Time: O(n^2), where n is number of rows
+// Space: O(n^2)
+function minimumTotalDP(triangle: number[][]): number {
+  let dp: number[][] = [];
+  dp.push([triangle[0][0]]);
+
+  for (let i = 1; i < triangle.length; i++) {
+    const sub: number[] = [];
+    for (let j = 0; j < triangle[i].length; j++) {
+      if (j === 0) {
+        sub.push(triangle[i][j] + dp[i - 1][j]);
+      } else if (j === triangle[i].length - 1) {
+        sub.push(triangle[i][j] + dp[i - 1][j - 1]);
+      } else {
+        sub.push(triangle[i][j] + Math.min(dp[i - 1][j], dp[i - 1][j - 1]));
+      }
+    }
+    dp.push(sub);
+  }
+
+  return Math.min(...dp[triangle.length - 1]);
 }
