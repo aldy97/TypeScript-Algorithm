c4540a3888a30a71552b0f8943d2e70ff1250a70
Feng Xiong <fengxiong34@gmail.com>
2021-04-21 09:54:51 -0400

    redo sort colors

--- src/medium/ColorSort.ts
+++ src/medium/ColorSort.ts
@@ -1,45 +1,22 @@
-//apparently this is not a good solution due to big space consumption
+// Inights: two-ptrs, where p0 is the right boundary of 0s, and p1 is the left boundary of 1s
+// Time: O(n)
+// Space: O(1)
 function sortColors(nums: number[]): void {
-  if (nums.length === 1) {
-    return;
-  }
-  let map = new Map<number, number>();
-  for (let num of nums) {
-    if (!map.has(num)) {
-      map.set(num, 1);
-    } else {
-      map.set(num, (map.get(num) as number) + 1);
-    }
-  }
-  for (let i = 0; i < nums.length; i++) {
-    if (map.get(0) !== 0 && map.get(0) !== undefined) {
-      nums[i] = 0;
-      map.set(0, (map.get(0) as number) - 1);
-    } else if (map.get(1) !== 0 && map.get(1) !== undefined) {
-      nums[i] = 1;
-      map.set(1, (map.get(1) as number) - 1);
-    } else {
-      nums[i] = 2;
-    }
-  }
-}
+  let p0 = 0;
+  let p1 = nums.length - 1;
+  let curr = 0;
 
-function sortColors2(nums: number[]): void {
-  let zero = 0;
-  let one = 0;
-  let two = 0;
-  for (let num of nums) {
-    num === 0 ? zero++ : num === 1 ? one++ : two++;
-  }
-  for (let i = 0; i < nums.length; i++) {
-    if (zero !== 0) {
-      nums[i] = 0;
-      zero--;
-    } else if (one !== 0) {
-      nums[i] = 1;
-      one--;
+  while (curr <= p1) {
+    if (nums[curr] === 0) {
+      const temp = nums[curr];
+      nums[curr++] = nums[p0];
+      nums[p0++] = temp;
+    } else if (nums[curr] === 2) {
+      const temp = nums[curr];
+      nums[curr++] = nums[p1];
+      nums[p1--] = temp;
     } else {
-      nums[i] = 2;
+      curr++;
     }
   }
 }
