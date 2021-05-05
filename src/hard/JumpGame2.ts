4e2124bb4ceb1a666fe6661c8ee64e99ac58a017
feng <fengxiong34@gmail.com>
2021-05-05 16:10:51 -0400

    redo JumpGameII

--- src/hard/JumpGame2.ts
+++ src/hard/JumpGame2.ts
@@ -1,14 +1,19 @@
-//Assumes that its for sure the end point is reachable.
+// Insights: always jump to the place that will take us the farthest
+// Time: O(n)
+// Space: O(1)
 function jumpGame2(nums: number[]): number {
   let end = 0;
   let furthestJump = 0;
   let jumps = 0;
+
   for (let i = 0; i < nums.length - 1; i++) {
     furthestJump = Math.max(furthestJump, nums[i] + i);
+    // if we have come to the end of the current jump, we need to make another jump:
     if (end === i) {
       jumps++;
       end = furthestJump;
     }
   }
+
   return jumps;
 }
