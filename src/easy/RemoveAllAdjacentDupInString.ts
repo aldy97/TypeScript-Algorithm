cef135e184afe43a441ab654b883195f0c620f98
Feng Xiong <fengxiong34@gmail.com>
2021-06-28 17:40:57 -0400

    modified

--- src/easy/RemoveAllAdjacentDupInString.ts
+++ src/easy/RemoveAllAdjacentDupInString.ts
@@ -2,14 +2,15 @@
 // Time: O(n), where n is the size of the string
 // Space: O(n)
 function removeAdjacentDuplicates(s: string): string {
-  const stack: string[] = [];
-  for (let i = 0; i < s.length; i++) {
-    if (stack.length && stack[stack.length - 1] == s[i]) {
-      stack.pop();
-    } else {
-      stack.push(s[i]);
-    }
-  }
+	const stack: string[] = [];
 
-  return stack ? stack.join("") : "";
+	for (let i = 0; i < s.length; i++) {
+		if (stack.length && stack[stack.length - 1] == s[i]) {
+			stack.pop();
+		} else {
+			stack.push(s[i]);
+		}
+	}
+
+	return stack.join("");
 }
